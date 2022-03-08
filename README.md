002a0577ede624faf68496c9b6ae526e4769528e
Frank Wu <31094470+frankuly@users.noreply.github.com>
2022-03-08 14:17:25 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+##cloud alibaba demo
+```
+springboot 2.3.12.RELEASE
+springcloud Hoxton.SR12
+springcloud alibaba 2.2.7.RELEASE
+nacos-server 2.0.3
+sentinel-dashboard 1.8.1
+```
---
+++ package/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO content_center.share (user_id,title,create_time,update_time,is_original,author,cover,summary,price,download_url,buy_count,show_flag,audit_status,reason) VALUES
+	 (1,'Spring Clound Alibaba 小程序实战1','2022-02-10 13:57:11','2022-02-10 13:57:11',0,'wufan','xxx','',0,'',1,0,'0',''),
+	 (1,'Spring Clound Alibaba 小程序实战2','2022-02-10 13:57:14','2022-02-10 13:57:14',0,'wufan','xxx','',0,'',1,0,'0',''),
+	 (1,'Spring Clound Alibaba 小程序实战3','2022-02-10 13:57:16','2022-02-10 13:57:16',0,'wufan','xxx','',0,'',1,0,'0','');
+	 
+
+INSERT INTO user_center.`user` (wx_id,wx_nickname,roles,avatar_url,create_time,update_time,bonus) VALUES
+	 ('asdasdas','wufan','admin','xxx','2022-02-10 13:37:43','2022-02-10 13:37:43',100),
+	 ('qqweqwe','wufan1','user','xxx','2022-02-10 13:37:48','2022-02-10 13:37:48',100);
